220d8aeb8f817fe79e2312c86499d58505464196
leonie-boullier <leonie.boullier@u-psud.fr>
2018-03-26 11:59:51 +0200

    Begin etablish form

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 
 from .models import Post
 
+from .forms import PostForm
+
 # Create your views here.
 
 def post_list(request):
@@ -16,3 +18,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+    
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
